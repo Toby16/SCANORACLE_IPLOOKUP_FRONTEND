4f8f895d7d4fc79f3af5c10c9a5df6b31e83a8a9
Toby 👹 <bolajibaasit14@gmail.com>
2026-05-22 12:39:36 +0100

    Update IPLookup.jsx

--- src/pages/scanoracle/IPLookup.jsx
+++ src/pages/scanoracle/IPLookup.jsx
@@ -527,7 +527,7 @@ export default function IPLookup() {
       </main>
 
       <footer className={styles.footer}>
-        ScanOracle™ - <span className={styles.footerAccent}>Ghostroute</span> Security 📡 - {new Date().getFullYear()}
+        ScanOracle™ - <span className={styles.footerAccent}>Ghostroute</span> Security - {new Date().getFullYear()} 📡
       </footer>
     </div>
   )
